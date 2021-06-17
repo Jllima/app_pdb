ea6ca76363bfb61a967357be80ce5620dcc4a139
Jorge Lima <jorgelima@mac.local>
2021-06-17 15:10:10 -0300

    Commit inicial

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community/eslint-config', // Default RN config
+    'standard-with-typescript', // Installed in step 2
+    'eslint-config-prettier', // Installed in step 3
+  ],
+  parser: '@typescript-eslint/parser', // Installed in step 2
+  plugins: [
+    '@typescript-eslint', // Installed in step 2
+    'react', // Installed in step 1
+    'react-native', // Installed in step 1
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json', // Required for Standard plugin
+  },
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+      },
+    ],
+    semi: ['error', 'never'],
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'no-redeclare': 'off',
+    'import/export': 'off',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# app_pdb
-Aplicativo mobile do Prontuário do Busão
