5fa1fb581573e4ca918881632dc179a9b39e1688
Jorge <jlimajorge@gmail.com>
2021-06-22 14:49:21 -0300

    chore: config contextApi

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import Routes from './routes/index'
 import {NavigationContainer} from '@react-navigation/native'
+import AuthContext from './presentation/contexts/auth'
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Routes />
+      <AuthContext.Provider value={{signed: false}}>
+        <Routes />
+      </AuthContext.Provider>
     </NavigationContainer>
   )
 }
--- src/presentation/pages/signin/signin.tsx
+++ src/presentation/pages/signin/signin.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Button} from 'react-native'
+import Styles from './styles'
 
 const SignIn: React.FC = () => {
   return (
-    <View>
-      <Text>Signin</Text>
+    <View style={Styles.container}>
+      <Button title="Entrar" onPress={() => {}} />
     </View>
   )
 }
