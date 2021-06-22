b7bd91016a41b9497d772f66525f18c86096d7c8
Jorge <jlimajorge@gmail.com>
2021-06-22 13:34:10 -0300

    feat: add rotas com react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import {View} from 'react-native'
+import Routes from './routes/index'
 import {NavigationContainer} from '@react-navigation/native'
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <View />
+      <Routes />
     </NavigationContainer>
   )
 }
---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,2 @@
+export {default as Login} from './menu/menu'
+export {default as SignIn} from './signin/signin'
