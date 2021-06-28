283fc31ee88f772b230e4539b7aa16d5db810fb9
Jorge <jlimajorge@gmail.com>
2021-06-28 11:13:12 -0300

    feat: fazendo login com valore passados por estado

--- src/presentation/pages/signin/signin.tsx
+++ src/presentation/pages/signin/signin.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {ActivityIndicator, Text} from 'react-native'
 import {Container, Input, Button, ButtonText, Logo, Icon} from './styles'
-// import AuthContext from '@pdb/presentation/contexts/auth'
 import {Authentication} from '@pdb/domain/usecases/auth/authentication'
 import {imgLogo} from '@pdb/presentation/assets'
 
@@ -9,38 +9,56 @@ type Props = {
 }
 
 const SignIn: React.FC<Props> = ({authentication}: Props) => {
-  // const {signed} = useContext(AuthContext)
+  const [state, setState] = useState({
+    identity: '',
+    password: '',
+    isLoading: false,
+    errorMessage: '',
+  })
 
   const handleSignIn = async (): Promise<void> => {
+    const {identity, password} = state
+    setState({...state, isLoading: true})
     try {
       const response = await authentication.auth({
-        identity: 'user_1',
-        password: '123abc',
+        identity: identity,
+        password: password,
       })
+      setState({...state, isLoading: false})
       console.log(response.auth_token)
     } catch (error: any) {
       const messageError: string = error.message
+      setState({...state, isLoading: false, errorMessage: messageError})
       console.log(`Error Auth: ${messageError}`)
     }
   }
 
   return (
     <Container>
+      <Text>{state.errorMessage}</Text>
       <Logo source={imgLogo} resizeMode="contain" />
       <Input
         placeholder="Matrícula"
         autoCorrect={false}
         autoCapitalize="none"
+        onChangeText={text => setState({...state, identity: text})}
       />
       <Input
         placeholder="Senha"
         autoCorrect={false}
         autoCapitalize="none"
         secureTextEntry
+        onChangeText={text => setState({...state, password: text})}
       />
       <Button onPress={handleSignIn}>
-        <ButtonText>Entrar</ButtonText>
-        <Icon name="log-in" size={20} color="#FFFFFF" />
+        {state.isLoading ? (
+          <ActivityIndicator color="#FFFFFF" />
+        ) : (
+          <>
+            <ButtonText>Entrar</ButtonText>
+            <Icon name="log-in" size={20} color="#FFFFFF" />
+          </>
+        )}
       </Button>
     </Container>
   )
