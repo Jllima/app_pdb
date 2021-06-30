220771445c2e386cad2bd390f555292b57f56e91
Jorge <jlimajorge@gmail.com>
2021-06-30 18:12:37 -0300

    chore: customização native base

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 import Routes from './routes/index'
 import {NavigationContainer} from '@react-navigation/native'
 import {AuthProvider} from './presentation/contexts/auth'
+import {StyleProvider} from 'native-base'
+import getTheme from '../native-base-theme/components'
+import platform from '../native-base-theme/variables/platform'
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </NavigationContainer>
+    <StyleProvider style={getTheme(platform)}>
+      <NavigationContainer>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </NavigationContainer>
+    </StyleProvider>
   )
 }
 
