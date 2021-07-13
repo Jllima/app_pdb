24237aeb5edaf980134f9361f5c8bfa3b2d0dd07
Jorge <jlimajorge@gmail.com>
2021-07-13 13:54:43 -0300

    feat: navegação para criar OS

--- src/routes/make-os.routes.tsx
+++ src/routes/make-os.routes.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import {MakeOs} from '@pdb/presentation/pages'
+import {MakeOs, FormOs} from '@pdb/presentation/pages'
 
 const MakeOsStack = createStackNavigator()
 
 const MakeOsRoutes: React.FC = () => {
   return (
-    <MakeOsStack.Navigator>
+    <MakeOsStack.Navigator initialRouteName="MakeOs">
       <MakeOsStack.Screen
         name="MakeOs"
         component={MakeOs}
         options={{headerShown: false}}
       />
+      <MakeOsStack.Screen name="FormOs" component={FormOs} />
     </MakeOsStack.Navigator>
   )
 }
