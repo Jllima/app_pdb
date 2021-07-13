19e5cc346b86f904b79fc5c76088746604d12af0
Jorge <jlimajorge@gmail.com>
2021-07-13 09:57:40 -0300

    refactor: nome do arquivo

--- src/domain/usecases/categories/get-problems-and-vehices.ts
+++ src/domain/usecases/categories/get-problems-and-vehices.ts
@@ -1,5 +1,5 @@
 import {ProblemsCategoriesAndVehiclesModel} from '@pdb/domain/models/list-problems-and-vehicles-model'
 
-export interface GetCategoriesAndVehicles {
+export interface GetProblemsAndVehicles {
   get: () => Promise<ProblemsCategoriesAndVehiclesModel>
 }
