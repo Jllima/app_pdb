9e7eb7713fe128801a2dda07d627d4d42a29c435
Jorge <jlimajorge@gmail.com>
2021-07-19 16:51:07 -0300

    feat: add rotas para o menu

--- src/presentation/pages/menu/menu.tsx
+++ src/presentation/pages/menu/menu.tsx
@@ -2,16 +2,26 @@ import React, {useContext} from 'react'
 import {HeaderApp, MenuButton} from '@pdb/presentation/components'
 import {Container} from './styles'
 import {AuthContext} from '@pdb/presentation/contexts'
+import {useNavigation} from '@react-navigation/native'
 
 const Menu: React.FC = () => {
   const {user} = useContext(AuthContext)
+  const navigation = useNavigation()
 
   return (
     <>
       <HeaderApp title={`Seja bem vindo(a): ${user.name}`} />
       <Container>
-        <MenuButton iconName="document-text-outline" iconText="Fazer OS" />
-        <MenuButton iconName="search-outline" iconText="Acompanhar OS" />
+        <MenuButton
+          iconName="document-text-outline"
+          iconText="Fazer OS"
+          onPress={() => navigation.navigate('MakeOsRoutes')}
+        />
+        <MenuButton
+          iconName="search-outline"
+          iconText="Acompanhar OS"
+          onPress={() => navigation.navigate('SearchOs')}
+        />
       </Container>
     </>
   )
